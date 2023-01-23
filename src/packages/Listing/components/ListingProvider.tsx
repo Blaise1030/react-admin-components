b213a6f18ef3a6efe1bd7af61cc0e04313f4b24f
Blaise1030 <61928648+Blaise1030@users.noreply.github.com>
2023-01-23 16:03:49 +0800

    fix: added query client

--- src/packages/Listing/components/ListingProvider.tsx
+++ src/packages/Listing/components/ListingProvider.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { ReactElement } from "react";
 import { ListingFilterProvider } from "./ListingFilterCore";
 
+const qC = new QueryClient();
 export default function ListingProvider<T extends object>({
   children,
 }: {
   children: ReactElement | ReactElement[];
 }) {
-  return <ListingFilterProvider<T>>{children}</ListingFilterProvider>;
+  return (
+    <ListingFilterProvider<T>>
+      <QueryClientProvider client={qC}>{children}</QueryClientProvider>
+    </ListingFilterProvider>
+  );
 }
